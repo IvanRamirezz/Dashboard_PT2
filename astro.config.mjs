dd1eff6abca4cb00f683ec19d9cbc8692a0d221c
IvanRamirezz <92224804+IvanRamirezz@users.noreply.github.com>
2026-04-20 09:02:59 -0600

    chore: switch adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
-import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
-    output: "server"
+  output: 'server',
+  adapter: vercel(),
 });
-
